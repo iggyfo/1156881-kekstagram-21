b25e22f084c37ace99eca2a84aa3beb934b806fd
iggyfo <iggyfo@gmail.com>
2020-10-25 10:49:00 +0300

    Правки в части валидации хэштегов

--- js/main.js
+++ js/main.js
@@ -87,7 +87,6 @@ drawPicture(descriptionArray);
 // module3-task2
 const CLASS_HIDDEN = `hidden`;
 const AVATAR_SIZE = 35;
-const body = document.querySelector(`body`);
 const firstPhoto = descriptionArray[0];
 const bigPicture = document.querySelector(`.big-picture`);
 const photo = bigPicture.querySelector(`.big-picture__img img`);
@@ -131,5 +130,216 @@ const newComment = (comments) => {
 };
 
 newComment(firstPhoto.comments);
-body.classList.add(`modal-open`);
-bigPicture.classList.remove(`hidden`);
+// document.body.classList.add(`modal-open`);
+// bigPicture.classList.remove(`hidden`);
+
+// Загрузка фотографии и открытие формы -----------------------------------------------------
+
+// Открываем и закрываем модальное окно
+const uploadOverlay = document.querySelector(`.img-upload__overlay`);
+const uploadFile = document.querySelector(`#upload-file`);
+const uploadCancel = document.querySelector(`#upload-cancel`);
+
+
+const showModal = () => {
+  document.body.classList.add(`modal-open`);
+  uploadOverlay.classList.remove(`hidden`);
+};
+
+const closeModal = () => {
+  document.body.classList.remove(`modal-open`);
+  uploadOverlay.classList.add(`hidden`);
+};
+
+uploadFile.addEventListener(`change`, () => {
+  showModal();
+
+  document.addEventListener(`keydown`, (evt) => {
+    // Дополнительно проверяем не является ли активными поля ввода хэштегов и комментариев.
+    // (по идее нужно получить состояние фокуса, но пока не понял как это реализовать)
+    if (evt.key === `Escape` && document.activeElement.name !== `hashtags` && document.activeElement.name !== `description`) {
+      closeModal();
+    }
+  });
+});
+
+uploadCancel.addEventListener(`click`, () => {
+  closeModal();
+});
+
+// Изменяем маштаб изображения
+const scaleControlSmaller = document.querySelector(`.scale__control--smaller`);
+const scaleControlBigger = document.querySelector(`.scale__control--bigger`);
+const scaleControlValue = document.querySelector(`.scale__control--value`);
+const imgPreview = document.querySelector(`.img-upload__preview`);
+
+let currentScaleValue = parseInt(scaleControlValue.value, 10);
+imgPreview.style.transform = `scale(` + currentScaleValue / 100 + `)`;
+
+scaleControlSmaller.addEventListener(`click`, () => {
+  if (currentScaleValue !== 0) {
+    currentScaleValue -= 25;
+    scaleControlValue.value = String(currentScaleValue) + `%`;
+    imgPreview.style.transform = `scale(` + String(currentScaleValue / 100) + `)`;
+  }
+});
+
+scaleControlBigger.addEventListener(`click`, () => {
+  if (currentScaleValue !== 100) {
+    currentScaleValue += 25;
+    scaleControlValue.value = String(currentScaleValue) + `%`;
+    imgPreview.style.transform = `scale(` + String(currentScaleValue / 100) + `)`;
+  }
+});
+
+// Добавляем эфекты
+const effectsRadio = document.querySelectorAll(`.effects__radio`);
+const effectPin = document.querySelector(`.effect-level__pin`);
+const effectLevelValue = document.querySelector(`.effect-level__value`);
+
+// Функция устанавливающая эфекты добавляя стили картинке при отпускании пина
+const getFilterPinUp = (effect) => {
+  effectPin.addEventListener(`mouseup`, () => {
+    switch (true) {
+      case (effect === `effect-none`):
+        imgPreview.style.filter = `none`;
+        break;
+      case (effect === `effect-chrome`):
+        imgPreview.style.filter = `grayscale(` + effectLevelValue.value / 100 + `)`;
+        break;
+      case (effect === `effect-sepia`):
+        imgPreview.style.filter = `sepia(` + effectLevelValue.value / 100 + `)`;
+        break;
+      case (effect === `effect-marvin`):
+        imgPreview.style.filter = `invert(` + effectLevelValue.value + `%)`;
+        break;
+      case (effect === `effect-phobos`):
+        imgPreview.style.filter = `blur(` + getEffectValue(effectLevelValue.value) + `px)`;
+        break;
+      case (effect === `effect-heat`):
+        imgPreview.style.filter = `brightness(` + getEffectValue(effectLevelValue.value) + `)`;
+        break;
+    }
+  });
+};
+
+// Обработчик изменения типа накладываемого эфекта
+effectsRadio.forEach((element) => {
+  element.addEventListener(`change`, () => {
+    // При изменении фильтра задаем картинке класс и стили оригинального изображения
+    imgPreview.className = `img-upload__preview`;
+    imgPreview.style.removeProperty(`filter`);
+
+    // if (element.id === `effect-none`) {
+    //   imgPreview.className = `img-upload__preview`;
+    //   getFilterPinUp(element.id);
+    // } else if (element.id === `effect-chrome`) {
+    //   imgPreview.classList.add(`effects__preview--chrome`);
+    //   getFilterPinUp(element.id);
+    // } else if (element.id === `effect-sepia`) {
+    //   imgPreview.classList.add(`effects__preview--sepia`);
+    //   getFilterPinUp(element.id);
+    // } else if (element.id === `effect-marvin`) {
+    //   imgPreview.classList.add(`effects__preview--marvin`);
+    //   getFilterPinUp(element.id);
+    // } else if (element.id === `effect-phobos`) {
+    //   imgPreview.classList.add(`effects__preview--phobos`);
+    //   getFilterPinUp(element.id);
+    // } else if (element.id === `effect-heat`) {
+    //   imgPreview.classList.add(`effects__preview--heat`);
+    //   getFilterPinUp(element.id);
+    // }
+    switch (true) {
+      case (element.id === `effect-none`):
+        imgPreview.className = `img-upload__preview`;
+        getFilterPinUp(element.id);
+        break;
+      case (element.id === `effect-chrome`):
+        imgPreview.classList.add(`effects__preview--chrome`);
+        getFilterPinUp(element.id);
+        break;
+      case (element.id === `effect-sepia`):
+        imgPreview.classList.add(`effects__preview--sepia`);
+        getFilterPinUp(element.id);
+        break;
+      case (element.id === `effect-marvin`):
+        imgPreview.classList.add(`effects__preview--marvin`);
+        getFilterPinUp(element.id);
+        break;
+      case (element.id === `effect-phobos`):
+        imgPreview.classList.add(`effects__preview--phobos`);
+        getFilterPinUp(element.id);
+        break;
+      case (element.id === `effect-heat`):
+        imgPreview.classList.add(`effects__preview--heat`);
+        getFilterPinUp(element.id);
+        break;
+    }
+  });
+});
+
+// Функция получает число от 1 до 100 и возвращает число от 1 до 3
+const getEffectValue = (value) => {
+  let ratio;
+  switch (true) {
+    case (value <= 33):
+      ratio = 1;
+      break;
+    case (value >= 66):
+      ratio = 3;
+      break;
+    default:
+      ratio = 2;
+  }
+  return ratio;
+};
+
+const inputHashtag = document.querySelector(`.text__hashtags`);
+const inputComment = document.querySelector(`.text__description`);
+const regex = /#[А-Яа-яA-Za-z0-9]{2,19}/g;
+const MAX_NUM_HASHTAGS = 5;
+const MAX_NUM_COMMENTS = 140;
+const MAX_NUM_CHAR = 20;
+
+const getNumDublicate = (arr, element) => {
+  let countDublicate = 0;
+  arr.forEach((arrElem) => {
+    if (arrElem === element) {
+      countDublicate++;
+    }
+  });
+  return countDublicate;
+};
+
+// Добавляем обработчик на поле ввода хэштега
+inputHashtag.addEventListener(`input`, () => {
+  const hashTags = inputHashtag.value.split(` `);
+  inputHashtag.setCustomValidity(``);
+  if (hashTags.length > MAX_NUM_HASHTAGS) {
+    inputHashtag.setCustomValidity(`Максимальное количество хэштегов - не более пяти`);
+  }
+
+  hashTags.forEach((element) => {
+    // Проверяем на максимальное количество знаков
+    if (element.length > MAX_NUM_CHAR) {
+      inputHashtag.setCustomValidity(`Максимальное количество знаков в хэштеге - не более 20 ` + element);
+    }
+    // Проверяем есть ли повторяющиеся хэштеги
+    if (getNumDublicate(hashTags, element) > 1) {
+      inputHashtag.setCustomValidity(`Хештеги не должны повторяться - удалите один из ` + element);
+    }
+    // Проверяем содержание хэштега
+    if (!regex.test(element)) {
+      inputHashtag.setCustomValidity(`Хештег ` + element + ` должен начинаться с # и состоять из букв/цифр`);
+    }
+  });
+});
+
+// Добавляем обработчик на поле ввода комментария
+inputComment.addEventListener(`input`, () => {
+  inputComment.setCustomValidity(``);
+  if (inputComment.textLength > MAX_NUM_COMMENTS) {
+    inputComment.setCustomValidity(`Длина комментария должна быть не более ` + MAX_NUM_COMMENTS + ` символов`);
+  }
+});
+
