a4ba6d36421e1cf7296000384168b2bad2e1a420
iggyfo <iggyfo@gmail.com>
2020-11-01 13:59:29 +0300

    Меняет назначение модулей

--- js/form.js
+++ js/form.js
@@ -1,174 +1,7 @@
 'use strict';
-// модуль работы с формами
+// модуль работы с формой
 
 (() => {
-  // Открываем и закрываем модальное окно
-  const uploadOverlay = document.querySelector(`.img-upload__overlay`);
-  const uploadFile = document.querySelector(`#upload-file`);
-  const uploadCancel = document.querySelector(`#upload-cancel`);
-
-
-  const showModal = () => {
-    document.body.classList.add(`modal-open`);
-    uploadOverlay.classList.remove(`hidden`);
-    document.addEventListener(`keydown`, closeModal);
-  };
-
-  const closeModal = (evt) => {
-    if (evt.key === `Escape` || evt.type === `click`) {
-      document.body.classList.remove(`modal-open`);
-      uploadOverlay.classList.add(`hidden`);
-    }
-    document.removeEventListener(`keydown`, closeModal);
-  };
-
-  uploadFile.addEventListener(`change`, showModal);
-  uploadCancel.addEventListener(`click`, closeModal);
-
-  // Изменяем маштаб изображения
-  const scaleControlSmaller = document.querySelector(`.scale__control--smaller`);
-  const scaleControlBigger = document.querySelector(`.scale__control--bigger`);
-  const scaleControlValue = document.querySelector(`.scale__control--value`);
-  const imgPreview = document.querySelector(`.img-upload__preview`);
-
-  let currentScaleValue = parseInt(scaleControlValue.value, 10);
-  imgPreview.style.transform = `scale(` + currentScaleValue / 100 + `)`;
-
-  scaleControlSmaller.addEventListener(`click`, () => {
-    if (currentScaleValue !== 0) {
-      currentScaleValue -= 25;
-      scaleControlValue.value = currentScaleValue + `%`;
-      imgPreview.style.transform = `scale(` + (currentScaleValue / 100) + `)`;
-    }
-  });
-
-  scaleControlBigger.addEventListener(`click`, () => {
-    if (currentScaleValue !== 100) {
-      currentScaleValue += 25;
-      scaleControlValue.value = currentScaleValue + `%`;
-      imgPreview.style.transform = `scale(` + (currentScaleValue / 100) + `)`;
-    }
-  });
-
-  // Добавляем эфекты
-  const effectsRadio = document.querySelectorAll(`.effects__radio`);
-  const effectPin = document.querySelector(`.effect-level__pin`);
-  const effectLeveldepth = document.querySelector(`.effect-level__depth`);
-
-
-  // const effectLevelValue = document.querySelector(`.effect-level__value`);
-
-  const onMouseDown = () => {
-    effectPin.addEventListener(`mousedown`, (downEvt) => {
-      downEvt.preventDefault();
-      // Rонстанта перемещаения на 1%
-      const VALUE_OF_ONE_SHIFT = 4.53;
-
-      // Получаем начальную координату Х
-      let startCoords = downEvt.clientX;
-
-      const onMouseMove = (moveEvt) => {
-        moveEvt.preventDefault();
-        // Вычисляем разницу между начальной и текущей координатой Х
-        let shift = startCoords - moveEvt.clientX;
-        // Перезаписываем начальную координату Х
-        startCoords = moveEvt.clientX;
-        // Переменная для применения стилей
-        let pinValue = (effectPin.offsetLeft - shift) / VALUE_OF_ONE_SHIFT;
-
-        if (pinValue >= 100) {
-          pinValue = 100;
-        } else if (pinValue <= 0) {
-          pinValue = 0;
-        }
-        effectPin.style.pinValue = pinValue + `%`;
-        effectLeveldepth.style.width = pinValue + `%`;
-      };
-
-      const onMouseUp = (upEvt) => {
-        upEvt.preventDefault();
-
-        document.removeEventListener(`mousemove`, onMouseMove);
-        document.removeEventListener(`mouseup`, onMouseUp);
-      };
-      document.addEventListener(`mousemove`, onMouseMove);
-      document.addEventListener(`mouseup`, onMouseUp);
-    });
-  };
-  onMouseDown();
-
-  // // Функция устанавливающая эфекты добавляя стили картинке при отпускании пина
-  // const getFilterPinUp = (effect) => {
-  //   effectPin.addEventListener(`mouseup`, () => {
-  //     switch (effect) {
-  //       case `effect-none`:
-  //         imgPreview.style.filter = `none`;
-  //         break;
-  //       case `effect-chrome`:
-  //         imgPreview.style.filter = `grayscale(` + effectLevelValue.value / 100 + `)`;
-  //         break;
-  //       case `effect-sepia`:
-  //         imgPreview.style.filter = `sepia(` + effectLevelValue.value / 100 + `)`;
-  //         break;
-  //       case `effect-marvin`:
-  //         imgPreview.style.filter = `invert(` + effectLevelValue.value + `%)`;
-  //         break;
-  //       case `effect-phobos`:
-  //         imgPreview.style.filter = `blur(` + getEffectValue(effectLevelValue.value) + `px)`;
-  //         break;
-  //       case `effect-heat`:
-  //         imgPreview.style.filter = `brightness(` + getEffectValue(effectLevelValue.value) + `)`;
-  //         break;
-  //     }
-  //   });
-  // };
-
-  // Обработчик изменения типа накладываемого эфекта
-  effectsRadio.forEach((element) => {
-    element.addEventListener(`change`, () => {
-      // При изменении фильтра задаем картинке класс и стили оригинального изображения
-      imgPreview.className = `img-upload__preview`;
-      imgPreview.style.removeProperty(`filter`);
-
-      switch (element.id) {
-        case `effect-none`:
-          imgPreview.className = `img-upload__preview`;
-          break;
-        case `effect-chrome`:
-          imgPreview.classList.add(`effects__preview--chrome`);
-          break;
-        case `effect-sepia`:
-          imgPreview.classList.add(`effects__preview--sepia`);
-          break;
-        case `effect-marvin`:
-          imgPreview.classList.add(`effects__preview--marvin`);
-          break;
-        case `effect-phobos`:
-          imgPreview.classList.add(`effects__preview--phobos`);
-          break;
-        case `effect-heat`:
-          imgPreview.classList.add(`effects__preview--heat`);
-          break;
-      }
-    });
-  });
-
-  // Функция получает число от 1 до 100 и возвращает число от 1 до 3
-  // const getEffectValue = (value) => {
-  //   let ratio;
-  //   switch (true) {
-  //     case (value <= 33):
-  //       ratio = 1;
-  //       break;
-  //     case (value >= 66):
-  //       ratio = 3;
-  //       break;
-  //     default:
-  //       ratio = 2;
-  //   }
-  //   return ratio;
-  // };
-
   const inputHashtag = document.querySelector(`.text__hashtags`);
   const inputComment = document.querySelector(`.text__description`);
   const regex = /^#[А-Яа-яA-Za-z0-9]{2,19}$/;
@@ -176,18 +9,8 @@
   const MAX_NUM_COMMENTS = 140;
   const MAX_NUM_CHAR = 20;
 
-  const getNumDublicate = (arr, element) => {
-    let countDublicate = 0;
-    arr.forEach((arrElem) => {
-      if (arrElem === element) {
-        countDublicate++;
-      }
-    });
-    return countDublicate;
-  };
-
   // Добавляем обработчик на поле ввода хэштега
-  inputHashtag.addEventListener(`input`, () => {
+  const validateHashtags = () => {
     const hashTags = inputHashtag.value.split(` `);
     inputHashtag.setCustomValidity(``);
     if (hashTags.length > MAX_NUM_HASHTAGS) {
@@ -202,7 +25,7 @@
         return;
       }
       // Проверяем есть ли повторяющиеся хэштеги
-      if (getNumDublicate(hashTags, tag) > 1) {
+      if (window.utils.getNumDublicate(hashTags, tag) > 1) {
         inputHashtag.setCustomValidity(`Хештеги не должны повторяться - удалите один из ` + tag);
         return;
       }
@@ -212,13 +35,18 @@
         return;
       }
     }
-  });
+  };
 
-  // Добавляем обработчик на поле ввода комментария
-  inputComment.addEventListener(`input`, () => {
+  const validateComment = () => {
     inputComment.setCustomValidity(``);
     if (inputComment.textLength > MAX_NUM_COMMENTS) {
       inputComment.setCustomValidity(`Длина комментария должна быть не более ` + MAX_NUM_COMMENTS + ` символов`);
     }
-  });
+  };
+  // Добавляем обработчик на поле ввода комментария
+
+  window.form = {
+    validateHashtags,
+    validateComment
+  };
 })();
--- js/picture.js
+++ js/picture.js
@@ -1,25 +1,58 @@
 'use strict';
-// отрисовка миниатюр
+// Создает фотографию пользователя с комментариями
 
 (() => {
-  const pictureTemplate = document.querySelector(`#picture`).content.querySelector(`.picture`);
-  const pictures = document.querySelector(`.pictures`);
+  const CLASS_HIDDEN = `hidden`;
+  const AVATAR_SIZE = 35;
+  const bigPicture = document.querySelector(`.big-picture`);
+  const photo = document.querySelector(`.big-picture img`);
+  const likesCount = bigPicture.querySelector(`.likes-count`);
+  const commentsCount = bigPicture.querySelector(`.comments-count`);
+  const socialComments = bigPicture.querySelector(`.social__comments`);
+  const socialCaption = bigPicture.querySelector(`.social__caption`);
+  const socialCommentCount = bigPicture.querySelector(`.social__comment-count`);
+  const commentsLoader = bigPicture.querySelector(`.comments-loader`);
 
-  // Создает объект из шаблона
-  function renderPictures(data) {
-    for (let i = 0; i < data.length; i++) {
-      const picture = pictureTemplate.cloneNode(true);
-      const pictureImg = picture.querySelector(`img`);
-      const pictureComments = picture.querySelector(`.picture__comments`);
-      const pictureLikes = picture.querySelector(`.picture__likes`);
-      pictureImg.src = data[i].url;
-      pictureComments.textContent = data[i].comments.length;
-      pictureLikes.textContent = data[i].likes;
-      pictures.appendChild(picture);
+  // Функция создения нового коментария
+  const createComments = (comments) => {
+    comments.forEach((element) => {
+      const fragment = document.createDocumentFragment();
+      const li = document.createElement(`li`);
+      li.classList.add(`social__comment`);
+
+      const img = document.createElement(`img`);
+      img.classList.add(`social__picture`);
+      img.src = element.avatar;
+      img.alt = element.name;
+      img.width = AVATAR_SIZE;
+      img.height = AVATAR_SIZE;
+      li.appendChild(img);
+
+      const p = document.createElement(`p`);
+      p.classList.add(`social__text`);
+      p.textContent = element.message;
+      li.appendChild(p);
+      fragment.appendChild(li);
+      socialComments.appendChild(fragment);
+    });
+  };
+
+  // Функция отрисовки большого изображения
+  const renderBigPicture = (data) => {
+    photo.src = data.url;
+    likesCount.textContent = data.likes;
+    commentsCount.textContent = data.comments.length;
+    socialCaption.textContent = data.description;
+
+    socialCommentCount.classList.add(CLASS_HIDDEN);
+    commentsLoader.classList.add(CLASS_HIDDEN);
+    while (socialComments.firstChild) {
+      socialComments.removeChild(socialComments.firstChild);
     }
-  }
+    createComments(data.comments);
+  };
 
   window.picture = {
-    renderPictures
+    renderBigPicture
   };
 })();
